020db98b1f1dee680a05955c526ca8c32ecd8dab
xRezio <edwin.carin@epitech.eu>
2023-10-05 14:02:35 +0200

    Update task04.php

--- task04.php
+++ task04.php
@@ -4,9 +4,9 @@ function whoami() {
     $age = isset($_POST['age']) && is_numeric($_POST['age']) ? $_POST['age'] : "no age";
 
     if ($name !== "I have no name" && $age !== "no age") {
-        echo "Hi, my name is $name and I’m $age years old.";
+        echo "Hi, my name is $name and I'm $age years old.";
     } elseif ($name === "I have no name" && $age !== "no age") {
-        echo "Hi, I have no name and I’m $age years old.";
+        echo "Hi, I have no name and I'm $age years old.";
     } elseif ($name !== "I have no name" && $age === "no age") {
         echo "Hi, my name is $name.";
     } else {
